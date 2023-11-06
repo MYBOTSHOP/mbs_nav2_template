114cbb2fc0a34c259f0f0e71a264ca1da23d3e7d
SOHAILMBS <salman.sohail@mybotshop.de>
2023-11-06 15:35:04 +0100

    Nav2 foxy template

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# mbs_nav2_template
-A Nav2 template ROS2 package
+# Quick Usage Instructions
+
+
+## Odom Navigation
+
+- Navigate in global frame of `map`
+
+```bash
+ros2 launch mbs_navigation odom_navi.launch.py
+```
+
+## Mapping
+
+```bash
+
+```
+
+## Localization
+
+```bash
+
+```
+
+## Map Navigations
+
+```bash
+
+```
+
+# Dependencies
+
+```bash
+sudo apt-get install ros-foxy-twist-mux\
+                     ros-foxy-navigation2\
+                     ros-foxy-nav2-bringup
+```
---
+++ launch/map_navi.launch.py
@@ -0,0 +1,64 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+    # Set sim param 
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    # Directories to launch files
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            get_package_share_directory('mbs_navigation'),
+            'maps',
+            'devshop.yaml'))
+
+    param_dir = LaunchConfiguration(
+        'params_file',
+        default=os.path.join(
+            get_package_share_directory('mbs_navigation'),
+            'param',
+            'nav2_map.yaml'))
+
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('diablo_viz'),
+        'rviz',
+        'nav2_view.rviz')
+    
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+    
+    # Launch Files and Nodes 
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=param_dir,
+            description='Full path to param file to load'),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params_file': param_dir}.items(),
+        ),
+    ])
